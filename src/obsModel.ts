6dc0bd2b0a67e6185a6754afdd16e6530569f29e
Sandi Chakravarty <sandi.newton@gmail.com>
2022-01-25 21:55:22 -0800

    wip scene / sources

--- src/obsModel.ts
+++ src/obsModel.ts
@@ -1,8 +1,7 @@
 import OBSWebSocket from "obs-websocket-js";
 
 const IMAGES = {
-	BG: 'https://images.unsplash.com/photo-1570937943292-a574bd5bc722?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8]',
-	BG2: 'https://images.unsplash.com/photo-1641582493393-b838bb4c1c6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3732&q=80',
+	BG: 'https://sandiz.github.io/qutopia-scorer/trivia-night.jpeg',
 }
 const SCENES: Record<string, boolean> = {
 	'Intro': false,
@@ -10,10 +9,16 @@ const SCENES: Record<string, boolean> = {
 	'Scores': false,
 };
 
+type Source = {
+	name: string;
+	typeId: string;
+	type: string;
+};
+
 class OBSModel {
 	obs: OBSWebSocket | undefined;
 
-	login(ip = 'localhost', port = 4444, password: string) {
+	login(ip = 'localhost', port = 4444, password = '') {
 		this.obs = new OBSWebSocket();
 		return this.obs.connect({ address: `${ip}:${port}`, password });
 	}
@@ -23,18 +28,24 @@ class OBSModel {
 	}
 
 	async createElements() {
+		if (!this.obs) { return; }
 		await this.createSceneElements();
-		await this.createIntroSourceElements();
+
+		const sources = await this.obs.send('GetSourcesList');
+		await this.createIntroSourceElements(sources.sources);
+		await this.createQuizSourceElements(sources.sources);
+
+		//await this.obs?.send('SetCurrentScene', { 'scene-name': 'Intro' });
 	}
 
 	async createSceneElements() {
 		if (!this.obs) { return; }
-		const list = await this.obs.send('GetSceneList');
+		const sceneList = await this.obs.send('GetSceneList');
 		const ids = Object.keys(SCENES);
 		for (let i = 0; i < ids.length; i++) {
 			const scene = ids[i];
 			if (SCENES[scene]) { return; }
-			SCENES[scene] = Boolean(list.scenes.find(s => s.name === scene));
+			SCENES[scene] = Boolean(sceneList.scenes.find(s => s.name === scene));
 			if (!SCENES[scene]) {
 				try {
 					await this.obs?.send('CreateScene', { 'sceneName': scene });
@@ -44,89 +55,72 @@ class OBSModel {
 				}
 			}
 		}
-		await this.obs?.send('SetCurrentScene', { 'scene-name': 'Intro' });
 	}
 
-	async createIntroSourceElements() {
+	async createIntroSourceElements(sources: Source[]) {
 		if (!this.obs) { return; }
-		const sources = await this.obs.send('GetSourcesList');
 
-		const imageBGSettings = {
+		await this.createBGSource('IntroBG', 'Intro', {
 			url: IMAGES.BG,
-			width: 1920,
-			height: 1080,
-		};
-		const hasBG = Boolean(sources.sources.find(s => s.name === 'BG'));
-		if (hasBG) {
-			this.obs.send('SetSourceSettings', {
-				sourceName: 'BG',
-				sourceSettings: imageBGSettings,
-			});
-		} else {
-			await this.obs.send('CreateSource', {
-				sourceKind: 'browser_source',
-				sourceName: 'BG',
-				sourceSettings: imageBGSettings,
-				sceneName: 'Intro',
-				setVisible: true,
-			});
-		}
-		const welcomeTextSettings = {
-			text: `General Quiz\nQM: Sayan Mohanty`,
-			color1: 4294945280,
-			color2: 4294945280,
-			font: {
-				face: "Helvetica",
-				flags: 0,
-				size: 70,
-				style: "Regular",
-				dropShadow: true,
-			},
-		};
-		const hasWelcomeText = Boolean(sources.sources.find(s => s.name === 'WelcomeText'));
-		if (hasWelcomeText) {
-			await this.obs.send('SetSourceSettings', {
-				sourceName: 'WelcomeText',
-				sourceSettings: {
-					...welcomeTextSettings,
-				}
-			});
-		} else {
-			await this.obs.send('CreateSource', {
-				sourceKind: 'text_ft2_source',
-				sourceName: 'WelcomeText',
-				sourceSettings: welcomeTextSettings,
-				sceneName: 'Intro',
-				setVisible: true,
-			});
-		}
-		const props = await this.obs.send('GetSceneItemProperties', {
-			"scene-name": "Intro",
-			item: { name: "WelcomeText" },
-		})
-		const { height, width } = props;
-		await this.obs.send('SetSceneItemProperties', {
-			"scene-name": "Intro",
-			item: {
-				name: "WelcomeText",
-			},
-			position: {
+		}, sources)
+
+		await this.createTextSource('WelcomeText', 'Intro', {
+			...TextSettings,
+			text: `General Quiz\nQM: Sayan Mohanty`
+		}, sources, ({ height }) => {
+			return {
 				x: 50,
-				y: 1080 - (height + 100),
-			},
-			scale: {
-				x: 1,
-				y: 1,
-			},
-			crop: {},
-			bounds: {},
+				y: 1000 - (height)
+			}
+		});
+	}
+
+	async createQuizSourceElements(sources: { name: string; typeId: string; type: string; }[]) {
+		if (!this.obs) { return; }
+		await this.createBGSource('QuizBG', 'Quiz', {
+			url: IMAGES.BG,
+		}, sources);
+
+		// score a team a
+		await this.createTextSource('TeamA', 'Quiz', {
+			...TextSettings,
+			text: `Fight Club`
+		}, sources, ({ height }) => {
+			return {
+				x: 50,
+				y: 1000 - (height) - 80,
+			}
+		});
+		await this.createTextSource('TeamAScore', 'Quiz', {
+			...TextSettings,
+			text: `100`
+		}, sources, ({ height }) => {
+			return {
+				x: 50,
+				y: 1000 - (height) 
+			}
 		});
 
-		/*	
-		this.obs.send('GetSourceSettings', { 'sourceName': 'Browser' }).then(data => {
-			console.log('GetSourceSettings', data);
+		// score b team b
+		await this.createTextSource('TeamB', 'Quiz', {
+			...TextSettings,
+			text: `Dequel`
+		}, sources, ({ width, height }) => {
+			console.warn(width)
+			return {
+				x: 1900 - width,
+				y: 1000 - (height) - 80,
+			}
 		});
-		*/
+		await this.createTextSource('TeamBScore', 'Quiz', {
+			...TextSettings,
+			text: `50`
+		}, sources, ({ width, height }) => {
+			return {
+				x: 1900 - width,
+				y: 1000 - (height) 
+			}
+		});
 	}
 
 	async getSourceSettings(sourceName: string) {
@@ -135,7 +129,76 @@ class OBSModel {
 		});
 		console.warn('source settings for', sourceName, settings);
 	}
+
+	async createTextSource(sourceName: string, sceneName: string, settings: {}, sources: Source[], position: (props: any) => { x: number, y:number } = () => ({ x: 0, y: 0 })) {
+		if (!this.obs) { return; }
+		const hasSource = Boolean(sources.find(s => s.name === sourceName));
+		if (hasSource) {
+			await this.obs.send('SetSourceSettings', {
+				sourceName,
+				sourceSettings: settings,
+			});
+		} else {
+			await this.obs.send('CreateSource', {
+				sourceKind: 'text_ft2_source',
+				sourceName,
+				sourceSettings: settings,
+				sceneName,
+				setVisible: true,
+			});
+		}
+
+		const props = await this.obs.send('GetSceneItemProperties', {
+			"scene-name": sceneName,
+			item: { name: sourceName },
+		})
+		await this.obs.send('SetSceneItemProperties', {
+			"scene-name": sceneName,
+			item: {
+				name: sourceName,
+			},
+			position: position(props),
+			scale: {
+				x: 1,
+				y: 1,
+			},
+			crop: {},
+			bounds: {},
+		});
+
+	}
+
+	async createBGSource(sourceName: string, sceneName: string, settings: {}, sources: Source[]) {
+		if (!this.obs) { return; }
+		const hasBG = Boolean(sources.find(s => s.name === 'IntroBG'));
+		if (hasBG) {
+			this.obs.send('SetSourceSettings', {
+				sourceName,
+				sourceSettings: settings,
+			});
+		} else {
+			await this.obs.send('CreateSource', {
+				sourceKind: 'browser_source',
+				sourceName,
+				sourceSettings: settings,
+				sceneName,
+				setVisible: true,
+			});
+		}
+	}
 }
 
+//const waitForIt = (n: number) => new Promise(resolve => setTimeout(resolve, n));
+const TextSettings = {
+	color1: 4294945280,
+	color2: 4294945280,
+	font: {
+		face: "Helvetica",
+		flags: 0,
+		size: 70,
+		style: "Regular",
+	},
+	drop_shadow: true
+}
 const OBS = new OBSModel();
 export default OBS;
